8c3ac58aa6caef8d90135f18b568a5f7f4c48980
LucasCamarero <l.camareroperez@ikasle.eus>
2025-11-04 21:00:18 +0100

    typography y Splash Screen

---
+++ app/src/main/java/com/example/di_tema4/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.di_tema4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.di_tema4.ui.theme.DI_Tema4Theme
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DI_Tema4Theme() { // Tema de Material 3
+                var showSplash by remember { mutableStateOf(true) }
+
+                if (showSplash) {
+                    SplashScreen(onTimeout = { showSplash = false })
+                } else {
+                    HomeScreen()
+                    //ShopScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            "Bienvenido al Mercado",
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
---
+++ app/src/main/java/com/example/di_tema4/ui/theme/Type.kt
@@ -0,0 +1,38 @@
+package com.example.di_tema4.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.di_tema4.R
+
+// Set of Material typography styles to start with
+val Typography2 = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.zalandosans_light)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.zalandosans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
