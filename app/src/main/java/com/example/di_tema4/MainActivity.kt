44fe718a97b687f7486353af89a6e57072268d34
LucasCamarero <l.camareroperez@ikasle.eus>
2025-11-09 12:04:59 +0100

    fix: modificada var ShowSplash para que funcione bien al rotar el movil

--- app/src/main/java/com/example/di_tema4/MainActivity.kt
+++ app/src/main/java/com/example/di_tema4/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import com.example.di_tema4.ui.theme.DI_Tema4Theme
 import androidx.compose.runtime.setValue
@@ -22,13 +23,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DI_Tema4Theme() { // Tema de Material 3
-                var showSplash by remember { mutableStateOf(true) }
+                var showSplash by rememberSaveable { mutableStateOf(true) }
 
                 if (showSplash) {
                     SplashScreen(onTimeout = { showSplash = false })
                 } else {
-                    HomeScreen()
-                    //ShopScreen()
+                    //HomeScreen()
+                    ShopScreen()
                 }
             }
         }
